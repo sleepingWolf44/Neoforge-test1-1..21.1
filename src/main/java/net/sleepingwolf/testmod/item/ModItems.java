622ea62d61d2c93f5ddd9d34f5e98fde10866f10
Kyler <44sleepingwolf@gmail.com>
2026-03-20 19:32:52 -0500

    got rid of redudancies

---
+++ src/main/java/net/sleepingwolf/testmod/item/ModItems.java
@@ -0,0 +1,10 @@
+package net.sleepingwolf.testmod.item;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.sleepingwolf.testmod.TestMod;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TestMod.MODID);
+
+
+}
